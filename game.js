c40b154179469c66c9cd05c2144b3bb626be370c
Elly M. iShmael <iraqgeeks@gmail.com>
2014-07-17 06:27:35 +0300

    more code

--- game.js
+++ game.js
@@ -15,12 +15,24 @@ var cell = function  (options) {
 }
 
 
-var grid = function () {
+var grid = function (options) {
 
 	var that = {};
+	that.width = options.width;
+	that.height = options.height;
+	that.w = options.w;
+
+	var col = that.width/that.w;
+	var row = that.height/that.w;
+
+	var cellArray = new Array(col)
+	for (var i = 0; i < cellArray.length; i++) {
+		cellArray[i] = new Array(row);
+	};
+
 	//populate the array with 0's and 1's
 	that.init = function () {
-		
+				
 	}
 
 	// generate new genration based on the previous generation
@@ -28,9 +40,11 @@ var grid = function () {
 		
 	}
 
+
 	that.draw = function  () {
-		// calling draw on each cell
+	
 	}
 
+	return that;
 
 }
