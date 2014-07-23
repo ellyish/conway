a6c0165ddf96f91f480eb39e4d17a480644fc352
Elly M. iShmael <iraqgeeks@gmail.com>
2014-07-23 04:26:16 +0300

    render function done

--- game.js
+++ game.js
@@ -6,10 +6,6 @@ var cell = function  (options) {
 	that.y = options.y;
 	that.w = options.w;
 	that.currentState = options.currentState;
-	that.nextState = options.nextState;
-	that.draw = function () {
-		ctx.drawRect(that.x, that.y, that.w, that.w);
-	}
 
 	return that;
 }
@@ -21,28 +17,46 @@ var grid = function (options) {
 	that.width = options.width;
 	that.height = options.height;
 	that.w = options.w;
+	that.ctx = options.ctx;
 
-	var col = that.width/that.w;
-	var row = that.height/that.w;
+	var col = Math.floor(that.width/that.w);
+	var row = Math.floor(that.height/that.w);
 
 	var cellArray = new Array(col)
 	for (var i = 0; i < cellArray.length; i++) {
 		cellArray[i] = new Array(row);
 	};
 
+	that.cellArray = cellArray;
 	//populate the array with 0's and 1's
 	that.init = function () {
-				
+		for (var i = 0; i < col; i++) {
+			for (var j = 0; j < row; j++) {
+				cellArray[i][j] = new cell({
+					x: i*that.w,
+					y: j*that.w,
+					w: that.w,
+					currentState: Math.round(Math.pow(Math.random(), 2))
+				})
+			};
+		};		
 	}
 
-	// generate new genration based on the previous generation
-	that.generate = function  () {
-		
+	// generate new genration based on the current generation
+	that.update = function  () {
+		//two large loops and one small loop
 	}
 
+	//draw the current grid
+	that.render = function  () {
+		for (var i = 0; i < col; i++) {
+			for (var j = 0; j < row; j++) {
 
-	that.draw = function  () {
-	
+				that.cellArray[i][j].currentState == 1 ? that.ctx.fillStyle = 'black' : that.ctx.fillStyle = 'white';
+				that.ctx.fillRect(that.cellArray[i][j].x , that.cellArray[i][j].y, that.cellArray[i][j].w - 1, that.cellArray[i][j].w  - 1);
+
+			};
+		};	
 	}
 
 	return that;
